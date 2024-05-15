02db1cd227220794de624139e2e7588fd2008c1a
Your Name <you@example.com>
2024-05-15 23:11:55 +0300

    Add project files.

---
+++ Models/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Models
+{
+    [Table("Tasks")]
+    public class TaskModel
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [DisplayName("Name of the task")]
+        [Required(ErrorMessage = "The Name field is required.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [DisplayName("Description of the task")]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
